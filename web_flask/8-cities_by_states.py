613335b8cca5f5973c8b50497b6daa4a0985dedb
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-08-18 23:07:10 +0200

    Update 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,10 +15,10 @@ def cities_by_states():
 
 
 @app.teardown_appcontext
-def remove_session(execption):
+def remove_session(exception):
     """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port='5000')
