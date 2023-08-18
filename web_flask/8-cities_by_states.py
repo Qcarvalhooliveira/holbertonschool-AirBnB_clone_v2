84597359550f3b436559d4a26db70fc69bb9bc8c
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-18 17:43:06 +0200

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,8 +11,12 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """List all cities by states"""
-    state = storage.all(State)
-    return render_template('8-cities_by_states.html', states=state)
+    states = storage.all(State).values()
+    result = []
+    for state in sorted(states, key=lambda x: x.name):
+        result.append([state, state.cities])
+    return render_template('8-cities_by_states.html', result=result)
+
 
 @app.teardown_appcontext
 def remove_session(execption):
@@ -21,4 +25,4 @@ def remove_session(execption):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
