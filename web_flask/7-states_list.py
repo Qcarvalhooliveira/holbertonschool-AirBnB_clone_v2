d932c8f8fd49921439a4e352b24674dcb415859f
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-08-18 22:49:17 +0200

    Update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_list():
+def states_list():
     """Display a HTML page"""
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def remove_session(execption):
+def remove_session(exception):
     """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port="5000")
